ff7d31c191da92f5ec98b3851072640b5a78df73
saltyypanda <esh7943@g.rit.edu>
2026-02-04 11:57:12 -0500

    Setting up terraform structure

---
+++ scripts/terraform_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install yum-utils
+sudo yum install -y yum-utils
+
+# Add the HashiCorp repository for Terraform
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+
+# Install Terraform
+sudo yum -y install terraform
+
+# Verify Terraform installation
+terraform -version
+
+# Configure AWS CLI (interactive)
+# aws configure
+
+# aws s3 cp s3://swen514-team5/ . --recursive

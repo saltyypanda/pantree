89be78063e5b537210af04bcef0db7629436257c
saltyypanda <esh7943@g.rit.edu>
2026-02-06 19:15:23 -0500

    Removed functionality to initialize db

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,16 +6,10 @@ echo "--- STARTING TERRAFORM ---"
 terraform init
 terraform apply -auto-approve
 
-cd ../
-
 # Write output to .env
 echo "--- WRITING ENV ---"
-./scripts/write_env.sh
+../scripts/write_env.sh
 
 # Create test user
 echo "--- CREATING TEST USER ---"
-./scripts/create_test_user.sh $(terraform output -raw user_pool_id) $(terraform output -raw user_pool_client_id)
-
-# Initialize database
-echo "--- INITIALIZING DATABASE ---"
-py ./scripts/init_db.py
+../scripts/create_test_user.sh $(terraform output -raw user_pool_id) $(terraform output -raw user_pool_client_id)
